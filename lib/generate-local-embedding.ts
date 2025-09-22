1add1e09048852c2773622b618e162c74c683ff8
Ariel Scarpinelli <triforcexp@gmail.com>
2025-09-22 15:13:07 -0300

    revert back to xenova to avoid exceeding function size limit

--- lib/generate-local-embedding.ts
+++ lib/generate-local-embedding.ts
@@ -4,6 +4,7 @@ export async function generateLocalEmbedding(content: string) {
   const generateEmbedding = await pipeline(
     "feature-extraction",
     "Snowflake/snowflake-arctic-embed-l-v2.0",
+    // @ts-ignore
     { dtype: "fp32", use_external_data_format: true }
   )
 
